79b5ef89f27d86d4034903eb19667ce4a7260781
atenaiis <attennaisc@gmail.com>
2021-04-13 13:08:00 -0500

    create friendship between two

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,2 +1,14 @@
 class FriendshipsController < ApplicationController
+before_action :authenticate_user!
+
+  def create
+    @friendship = current_user.friendships.build
+    @friendship.friend_id = params[:user_id]
+   
+    if @friendship.save
+      redirect_to users_path, notice: 'Invite was successfully sent.'
+    else
+      redirect_to users_path, alert: 'Invite was not sent.'
+    end
+  end
 end
